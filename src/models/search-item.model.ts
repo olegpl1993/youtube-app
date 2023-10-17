39bfc7fb5476f4b403e1164194602b276f34dd83
Oleg <olegpl1993@gmail.com>
2023-10-18 02:50:52 +0300

    feat: create models

---
+++ src/models/search-item.model.ts
@@ -0,0 +1,73 @@
+export interface SearchItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  defaultAudioLanguage: string;
+  defaultLanguage?: string;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium: Medium;
+  high: High;
+  standard: Standard;
+  maxres: Maxres;
+}
+
+export interface Default {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Medium {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface High {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Standard {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Maxres {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Localized {
+  title: string;
+  description: string;
+}
+
+export interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
