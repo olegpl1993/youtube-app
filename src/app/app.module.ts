401bb229d0df0e1b8993df165069a80f4c457888
Oleg <olegpl1993@gmail.com>
2023-10-27 01:08:37 +0300

    init: install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
 import HeaderModule from './header/header.module';
@@ -13,6 +14,7 @@ import SearchResultListModule from './search-result-list/search-result-list.modu
     AppRoutingModule,
     HeaderModule,
     SearchResultListModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
