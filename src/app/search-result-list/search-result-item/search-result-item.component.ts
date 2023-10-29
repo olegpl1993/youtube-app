7d3d59a881c9fd23be43646895e30c30836e9ce7
Oleg <olegpl1993@gmail.com>
2023-10-29 21:01:14 +0300

    feat: color row from published date

--- src/app/search-result-list/search-result-item/search-result-item.component.ts
+++ src/app/search-result-list/search-result-item/search-result-item.component.ts
@@ -9,7 +9,21 @@ import { SearchItem } from 'src/models/search-result.model';
 export default class SearchResultItemComponent {
   @Input() public searchItem: SearchItem | null = null;
 
+  public getColor(): string | null {
+    const publishedAt = this.searchItem?.snippet?.publishedAt || '';
+    const currentDate = new Date();
+    const publishedDate = new Date(publishedAt);
+    const timeDifference = currentDate.getTime() - publishedDate.getTime();
+    const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+
+    if (daysDifference > 180) return 'red';
+    if (daysDifference >= 30) return 'yellow';
+    if (daysDifference >= 7) return 'green';
+    return 'blue';
+  }
+
   public test(): void {
     console.log('test');
+    console.log(this.getColor());
   }
 }
