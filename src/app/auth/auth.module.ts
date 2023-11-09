7d377e73c64fc31f1fcae2144eb0ab2d24313409
Oleg <olegpl1993@gmail.com>
2023-11-10 00:15:52 +0200

    feat: auth layout

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import ButtonComponent from 'src/shared/button/button.component';
 import AuthComponent from './components/auth/auth.component';
 import AuthRoutingModule from './auth-routing.module';
 
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, ButtonComponent],
   exports: [AuthComponent],
 })
 export default class AuthModule {}
--- src/app/auth/components/auth/auth.component.ts
+++ src/app/auth/components/auth/auth.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export default class AuthComponent {
+  private login = '';
+  private password = '';
 
+  handleSubmit(event: Event) {
+    event.preventDefault();
+    console.log('submit');
+  }
+
+  handleLoginInput(event: Event) {
+    if (event.target instanceof HTMLInputElement)
+      this.login = event.target.value.trim();
+
+    console.log(this.login);
+  }
+
+  handlePasswordInput(event: Event) {
+    if (event.target instanceof HTMLInputElement)
+      this.password = event.target.value.trim();
+
+    console.log(this.password);
+  }
 }
