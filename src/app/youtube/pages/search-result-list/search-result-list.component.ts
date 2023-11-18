6f472b5dbf98e63401cdca90996a9fc9c19147ef
Oleg <olegpl1993@gmail.com>
2023-11-18 03:12:04 +0200

    feat: unsubscribe in details and search-list components

--- src/app/youtube/pages/search-result-list/search-result-list.component.ts
+++ src/app/youtube/pages/search-result-list/search-result-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SearchItem } from 'src/shared/models/search-result.model';
 import SortKey from 'src/shared/enums/sort-key.enum';
+import { Subscription } from 'rxjs';
 import YoutubeService from '../../services/youtube.service';
 
 @Component({
@@ -8,24 +9,35 @@ import YoutubeService from '../../services/youtube.service';
   templateUrl: './search-result-list.component.html',
   styleUrls: ['./search-result-list.component.scss'],
 })
-export default class SearchResultListComponent implements OnInit {
+export default class SearchResultListComponent implements OnInit, OnDestroy {
   public searchResults: SearchItem[] = [];
   public sortKey: SortKey | null = null;
   public sortingInput = '';
+  private subscriptions: Subscription[] = [];
 
   constructor(private youtubeService: YoutubeService) {}
 
   ngOnInit() {
-    this.youtubeService.renderData$.subscribe((searchResults) => {
-      this.searchResults = searchResults;
-    });
+    this.subscriptions.push(
+      this.youtubeService.renderData$.subscribe((searchResults) => {
+        this.searchResults = searchResults;
+      })
+    );
 
-    this.youtubeService.sortKey$.subscribe((sortKey) => {
-      this.sortKey = sortKey;
-    });
+    this.subscriptions.push(
+      this.youtubeService.sortKey$.subscribe((sortKey) => {
+        this.sortKey = sortKey;
+      })
+    );
 
-    this.youtubeService.sortingInput$.subscribe((sortingInput) => {
-      this.sortingInput = sortingInput;
-    });
+    this.subscriptions.push(
+      this.youtubeService.sortingInput$.subscribe((sortingInput) => {
+        this.sortingInput = sortingInput;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
