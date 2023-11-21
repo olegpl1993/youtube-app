5514919972e162b6fcf6719d785f2ffe84a73235
Oleg <olegpl1993@gmail.com>
2023-11-21 02:22:42 +0200

    feat: admin page layput

--- src/app/admin/components/admin/admin.component.ts
+++ src/app/admin/components/admin/admin.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export default class AdminComponent {}
+export default class AdminComponent {
+  public createCardForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.createCardForm = this.fb.group({
+      title: ['', [Validators.required]],
+      discription: ['', [Validators.required]],
+      img: ['', [Validators.required]],
+      linkVideo: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.createCardForm.value);
+  }
+}
