969c56bca93679c4a9ca6b8cead0908f8c69eb92
Oleg <olegpl1993@gmail.com>
2023-11-23 01:14:50 +0200

    feat: createCardForm

--- src/app/admin/components/admin/admin.component.ts
+++ src/app/admin/components/admin/admin.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -10,14 +17,31 @@ export default class AdminComponent {
   public createCardForm: FormGroup;
   constructor(private fb: FormBuilder) {
     this.createCardForm = this.fb.group({
-      title: ['', [Validators.required]],
-      discription: ['', [Validators.required]],
+      title: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      discription: ['', [Validators.maxLength(255)]],
       img: ['', [Validators.required]],
       linkVideo: ['', [Validators.required]],
+      creationDate: ['', [Validators.required, this.futureDateValidator()]],
     });
   }
 
+  futureDateValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const selectedDate = new Date(control.value);
+      const currentDate = new Date();
+      currentDate.setHours(23, 59, 59, 999);
+      return selectedDate < currentDate ? null : { futureDate: true };
+    };
+  }
+
   onSubmit() {
-    console.log(this.createCardForm.value);
+    console.log(this.createCardForm);
   }
 }
